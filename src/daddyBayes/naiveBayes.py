4cbd5d5912a1279262a84a4051ea9424b287bca7
theArctesian <stephen.d.okita@gmail.com>
2022-07-26 09:53:20 -0700

    init

--- src/daddyBayes/naiveBayes.py
+++ src/daddyBayes/naiveBayes.py
@@ -10,13 +10,51 @@ Word,Senti
                                       "sell" : 1,
                                       "spree" : 1
                                     }
+thisdict.update({"year": 2020}) 
+thisdict["year"] = 2018
+x = thisdict.keys() 
 
 nive bayes = oc of word / all words
 
 """
+from coalas import importCSV as c
 
-def clean():
+def clean(text):
+    text = str(text)
+    text = re.sub('[^A-Za-z]+', ' ', text).lower().strip()
+    return text
 
-def removeStopWords():
-def tokenize(sentence)
-def train
+def tokenize(text):
+    text = clean(text)
+    text = text.split(" ")
+    return text
+
+
+def lemma(text):
+    text = tokenize(text)
+    text = [word for word in text if not word in stop_words]
+    lemma = " ".join(text)
+    lemma = lemma.lower()
+    return lemma
+
+def setup(dictCount):
+    for i in dictCount:
+      globals()[f'dict{i}'] = {}
+
+def train(x,y): # Input,Output,Outputs or how many dict are to be made
+    for i in range(len(y)):  
+        if y[i] == "-1":
+            Neg[x[i]] +=1
+        if y[i] == "0":
+            Neu[x[i]] +=1
+        if y[i] == "1":
+            Pos[x[i]] +=1
+
+
+
+if __name__ == "__main__":
+    c.importCSV("../../tests/trained.csv")
+    Neg = {}
+    Neu = {}
+    Pos = {}
+    train(c.lemma, c.trained)
